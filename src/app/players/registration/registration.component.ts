01ceb72162966387af30ac762926d8b37ecce571
SIMPLURIS\shassaninia <shassaninia@simpluris.com>
2017-03-22 17:42:18 -0400

    binding registration view model to registration form

--- src/app/players/registration/registration.component.ts
+++ src/app/players/registration/registration.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { RegistrationViewModel } from "./registrationViewModel";
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
-export class RegistrationComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class RegistrationComponent {
+  registrationViewModel : RegistrationViewModel;
+  constructor() { 
+    this.registrationViewModel = new RegistrationViewModel();
   }
 
+  addOrRemoveInjury(value: string){
+
+    if(this.confirmInjuryNotAlreadyChosen(value))
+    {
+        this.registrationViewModel.injuries.push(value);
+    }
+    else
+    {
+      let index =  this.registrationViewModel.injuries.indexOf(value);
+      this.registrationViewModel.injuries.splice(index);
+    }
+    
+  }
+
+  private confirmInjuryNotAlreadyChosen(value:string):boolean{
+    return this.registrationViewModel.injuries.find(injury => injury.toLowerCase() === value.toLowerCase()) == null;
+  }
 }
